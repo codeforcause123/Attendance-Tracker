7dee5b57a2954369cd2c09dab6201344f8ad0d7a
Aayush <aayush.130702@gmail.com>
2023-09-17 19:28:44 +0530

    Updated Links

--- frontend/src/components/UserForm.js
+++ frontend/src/components/UserForm.js
@@ -10,7 +10,7 @@ function UserForm({ formLogin }) {
     event.preventDefault();
     try {
       const response = await fetch(
-        "http://localhost:4000/api/acadmia",
+        "https://attendance-tracker-baackend.vercel.app/api/acadmia",
         {
           method: "POST",
           headers: {

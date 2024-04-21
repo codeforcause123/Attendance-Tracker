8acaa2e14db91941f460ca22f7b0fca918723250
Aayush <aayush.130702@gmail.com>
2024-04-21 17:08:21 +0530

    added Analytics

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,18 @@ import UserForm from "./components/UserForm";
 import UserData from "./components/UserData";
 import Error from "./components/Error";
 import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react";
 const App = () => {
   return (
     <BrowserRouter>
       <AnimatePresence mode="wait">
         <Routes>
-          <Route
-            path="/"
-            element={<UserForm />}
-          />
+          <Route path="/" element={<UserForm />} />
           <Route path="/youratt" element={<UserData />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </AnimatePresence>
+      <Analytics />
     </BrowserRouter>
   );
 };
